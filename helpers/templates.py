05f638035358ea377837b361eb38093d7b10ecf1
Nikita Chursin <nchursin@aquivalabs.com>
2017-04-03 11:22:53 +0300

    fixed currently existing templates in py

--- helpers/templates.py
+++ helpers/templates.py
@@ -6,6 +6,6 @@ class TemplateGetter():
 	def getTemplates():
 		return {
 			'other/ClassProperty': "{{access}} {${'static' if static else ''}$} {{valueType}} {{propName}}{${' { get; set; }' if createGetSet else ';'}$}",
-			'other/Method': "{${ '// {{comment}}' if comment else '' }$}\n\t{{access}} {${'static' if static else ''}$} {{returnType}} {{methodName}} ({{methodArguments}}){\n\t\t{${'' if 'void' == returnType else '{{returnType}} result;'}$}\n\t\t{${'// TODO: {{todo_comment}}' if todo_comment else ''}$}\n\t\t{${'' if 'void' == returnType else 'return result;'}$}\t\t}",
+			'other/Method': "{${ '// {{comment}}' if comment else '' }$}\n\t{{access}} {${'static' if static else ''}$} {{returnType}} {{methodName}} ({{methodArguments}}){\n\t\t{${'' if 'void' == returnType else '{{returnType}} result;'}$}\n\t\t{${'// TODO: {{todo_comment}}' if todo_comment else ''}$}\n\t\t{${'' if 'void' == returnType else 'return result;'}$}\n\t\t}",
 			'other/SimpleClassTemplate': "\n{{intends}}{{access}} {${'{{classType}}' if classType else ''}$} class {{className}}{${'' if not extends else ' extends {{extends}}'}$}{${'' if not implements else ' implements {{implements}}'}$} {\n\n{${'\\n'.join([('	{{intends}}' + prop) for prop in properties])}$}\n\n\t\t{{methods}}\n\n{{intends}}}"
 		}
